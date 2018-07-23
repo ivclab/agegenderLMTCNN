1d4dc39ecc50d9c60a016e1d9920400dad0724cb
Jia-HongHenryLee <honghenry.lee@gmail.com>
2018-07-23 15:53:48 +0800

    add models

---
+++ download_model.py
@@ -0,0 +1,83 @@
+# Copyright 2017 The BEGAN-tensorflow Authors(Taehoon Kim). All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+# MIT License
+#
+# Modifications copyright (c) 2018 Image & Vision Computing Lab, Institute of Information Science, Academia Sinica
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ==============================================================================
+
+import requests
+import zipfile
+import os
+
+def download_file_from_google_drive(id, destination):
+
+	URL = "https://docs.google.com/uc?export=download"
+
+	session = requests.Session()
+
+	response = session.get(URL, params = {'id':id}, stream=True)
+	token = get_confirm_token(response)
+
+	if token:
+		params = {'id': id, 'confirm':token}
+		response = session.get(URL, params=params, stream=True)
+
+	save_response_content(response, destination)
+
+def get_confirm_token(response):
+
+	for key, value in response.cookies.items():
+		if key.startswith('download_warning'):
+			return value
+
+	return None
+
+def save_response_content(response, destination):
+
+	CHUNK_SIZE = 32768
+
+	with open(destination, "wb") as f:
+		for chunk in response.iter_content(CHUNK_SIZE):
+			if chunk: # filter out keep-alive new chunks
+				f.write(chunk)
+
+if __name__ == "__main__":
+
+	print ('Download Model files ... ')
+	file_id = '1C0UvEAthKRcnvatHOZAHshPldRiJZnfz'
+	destination = 'models.zip'
+	download_file_from_google_drive(file_id, destination)
+	with zipfile.ZipFile(destination) as zf:
+		zip_dir = zf.namelist()[0]
+		zf.extractall('./')
+	os.remove(destination)
---
+++ script/evalfold3_best.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+python eval.py \
+--multitask True \
+--model_type LMTCNN-2-1 \
+--eval_dir ./tfrecord/train_val_test_per_fold_agegender/test_fold_is_2 \
+--eval_data test \
+--model_dir ./models/train_val_test_per_fold_agegender/test_fold_is_2/LMTCNN-2-1-run-29437 \
+--result_dir ./results \
+--batch_size 128
